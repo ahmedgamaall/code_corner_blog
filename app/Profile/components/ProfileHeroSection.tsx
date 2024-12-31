33eaf8b726b99b605078d5ea7f6250c00cca20aa
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-12-31 15:31:32 +0300

    profile page ui done

--- app/Profile/components/ProfileHeroSection.tsx
+++ app/Profile/components/ProfileHeroSection.tsx
@@ -1,21 +1,35 @@
+import Image from "next/image";
+
 export default function ProfileHeroSection() {
   return (
     <div
-      className="w-full mb-2 flex flex-col space-y-4 px-72 justify-evenly items-center py-8"
+      className="w-full mb-2 pt-16 flex flex-col space-y-4 px-72 justify-evenly items-center py-8"
       style={{ backgroundColor: "#B3CFE1" }}
     >
-      <h1 className="font-black text-4xl" style={{ color: "#172B4D" }}>
-        Categories
+      <Image
+        className="block rounded-full mx-auto h-36 w-36 object-cover"
+        src="/Ahmed.jpg"
+        width={400}
+        height={400}
+        alt="user image"
+      />
+      <h1 className="font-black text-3xl" style={{ color: "#172B4D" }}>
+        Ahmed Gamal
       </h1>
-      <p className="text-lg text-center" style={{ color: "#172B4D" }}>
-        Front-End Development, Back-End Development, Full-Stack Development,
-        Mobile App Development, Data Science and Machine Learning, Game
-        Development, Web Development, Cyber security, Software Engineering
+      <p className="text-xl text-center" style={{ color: "#172B4D" }}>
+        Where dreams turn into codes
       </p>
-      <p className="text-lg text-center" style={{ color: "#172B4D" }}>
-        By specializing in a particular area, programmers can become more
-        proficient, valuable, and in-demand in the job market.
-      </p>
+      <div className="flex flex-row space-x-4">
+        <p className="text-lg text-center" style={{ color: "#172B4D" }}>
+          Job Title :
+        </p>
+        <h5
+          className="text-lg font-bold text-center"
+          style={{ color: "#172B4D" }}
+        >
+          Front-end Specialists
+        </h5>
+      </div>
     </div>
   );
 }
--- app/Profile/page.tsx
+++ app/Profile/page.tsx
@@ -1,12 +1,15 @@
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar";
+import ProfileBodySection from "./components/ProfileBodySection";
 import ProfileHeroSection from "./components/ProfileHeroSection";
 
 export default function Profile() {
   return (
-  <div className="flex flex-col w-full items-center justify-items-center font-[family-name:var(--font-geist-sans)] justify-stretch ">
-    <NavBar/>
-    <ProfileHeroSection />
-    <Footer/>
-    </div>  );
+    <div className="flex flex-col w-full items-center justify-items-center font-[family-name:var(--font-geist-sans)] justify-stretch ">
+      <NavBar />
+      <ProfileHeroSection />
+      <ProfileBodySection />
+      <Footer />
+    </div>
+  );
 }
