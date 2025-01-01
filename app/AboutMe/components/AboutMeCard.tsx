cdc07456d79367965d098b724f9cf1ab65d36f62
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2025-01-01 16:47:10 +0300

    about me page done

--- app/AboutMe/components/AboutMeCard.tsx
+++ app/AboutMe/components/AboutMeCard.tsx
@@ -1,25 +1,36 @@
 type AboutMeCardProps = {
   title: string;
-  content: string;
+  content?: string;
   image: string;
   direction?: string;
+  children?: React.ReactNode;
 };
-export default function AboutMeCard({ title, content, image, direction = "left" }: AboutMeCardProps) {
+export default function AboutMeCard({
+  title,
+  content,
+  image,
+  direction = "left",
+  children = null,
+}: AboutMeCardProps) {
   return (
     <div
       className={
         direction === "left"
-          ? "w-full flex flex-row px-40 justify-evenly items-center py-10"
-          : "w-full flex flex-row-reverse px-40 justify-evenly items-center py-10"
+          ? "w-full flex flex-row px-40 justify-between items-center py-10"
+          : "w-full flex flex-row-reverse px-40 justify-between items-center py-10"
       }
     >
       <div className="flex flex-col py-4 justify-around space-y-4 mx-12">
         <h2 className="text-4xl font-bold" style={{ color: "#172B4D" }}>
           {title}
         </h2>
-        <p className="text-xl" style={{ color: "#172B4D" }}>
-          {content}
-        </p>
+        {children === null ? (
+          <p className="text-xl" style={{ color: "#172B4D" }}>
+            {content}
+          </p>
+        ) : (
+          children
+        )}
       </div>
 
       <img className="mx-auto w-1/3 rounded-lg" src={image} alt="About Me" />
--- app/AboutMe/page.tsx
+++ app/AboutMe/page.tsx
@@ -1,21 +1,118 @@
+import Link from "next/link";
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar";
 import AboutMeCard from "./components/AboutMeCard";
 import AboutMeHeroSection from "./components/AboutMeHeroSection";
 
 export default function AboutMe() {
+  const softSkills = [
+    " Strong attention to detail and ability to deliver high-quality work.",
+    "Good communication and teamwork skills.",
+    "Strong problem-solving skills.",
+    "Time management.",
+    "Adaptability.	Continuous learning.",
+    "Collaboration.",
+    "Creativity.",
+    "Strong work ethic.",
+    "Communication skills.",
+    "Positive attitude.",
+  ];
+
+  const softSkillsContent = softSkills.map((skill) => (
+    <li className="text-xl" style={{ color: "#172B4D" }}>
+      - {skill}
+    </li>
+  ));
+
+  const technicalSkills = [
+    " Dart Programming Language",
+    "State Management (Bloc)",
+    "RESTful APIs",
+    "Firebase (or other relevant cloud platforms)",
+    "Figma, Adobe XD	Flutter Development(Android & iOS)",
+    "Git, GitHub, GitLab",
+    "Geolocation and Maps Integration",
+    "Audio and Video Playback",
+  ];
+  const technicalSkillsContent = technicalSkills.map((skill) => (
+    <li className="text-xl" style={{ color: "#172B4D" }}>
+      - {skill}
+    </li>
+  ));
+  const languages = ["Arabic (Native Language) ", "English - good"];
+  const languagesContent = languages.map((language) => (
+    <li className="text-xl" style={{ color: "#172B4D" }}>
+      - {language}
+    </li>
+  ));
   return (
     <div className="flex flex-col w-full items-center justify-items-center font-[family-name:var(--font-geist-sans)] justify-stretch ">
       <NavBar />
       <AboutMeHeroSection />
       <AboutMeCard
         image="/Ahmed.jpg"
-        title="Who am I?"
-        content="What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a 
-type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including 
-versions of Lorem Ipsum."
+        title="Who am I ?"
+        content="I am Ahmed Jamal, originally from Qena, Egypt. A results-oriented and ambitious individual, I am calm, kind, and possess a patient and understanding nature. I am open-minded and a people person who loves meeting new people and exchanging ideas.
+Having been a professional runner in the past, I am passionate about sports, particularly swimming and running. This experience has cultivated discipline, perseverance, and a strong work ethic within me.
+I possess excellent communication, public speaking, and critical thinking skills, making me a valuable asset in team-oriented environments. I am eager to apply my skills and knowledge to new challenges."
       />
+      <AboutMeCard
+        image="/AhmedInFrontOfDoor.jpg"
+        title="Projects"
+        direction="right"
+      >
+        <div>
+          <p className="text-xl" style={{ color: "#172B4D" }}>
+            Story time web platform: is a Flutter web application that provides
+            users with a convenient platform to browse and view stories. The app
+            utilizes Firebase to fetch story data, and enable user interactions.
+            It incorporates the BLoC pattern to manage state and abstract the
+            data layer.
+          </p>
+          <Link
+            href="https://github.com/ahmedgamaall/story_time"
+            className="text-xl hover:underline hover:text-orange-600"
+            style={{ color: "#172B4D" }}
+          >
+            ( source code )
+          </Link>
+        </div>
+      </AboutMeCard>
+      <AboutMeCard image="/Ahmed.jpg" title="Technical Skills">
+        <ul>{technicalSkillsContent}</ul>
+      </AboutMeCard>
+      <AboutMeCard
+        image="/AhmedInFrontOfDoor.jpg"
+        direction="right"
+        title="Soft Skills"
+      >
+        <ul>{softSkillsContent}</ul>
+      </AboutMeCard>
+      <AboutMeCard
+        image="/Ahmed.jpg"
+        title="Education"
+        content="Bachelor's Degree, Accounting
+                University of El-Gzira Academy, Qena
+                2013 - 2017"
+      />
+      <AboutMeCard
+        image="/AhmedInFrontOfDoor.jpg"
+        direction="right"
+        title="Languages"
+      >
+        <div className="w-full">
+          <ul>{languagesContent}</ul>
+        </div>
+      </AboutMeCard>
+      <AboutMeCard image="/Ahmed.jpg" title="Certifications">
+        <Link
+          href="https://www.udemy.com/certificate/UC-cb40e7df-0c88-4100-862f-ddcd5871374b/"
+          className="text-xl hover:underline hover:text-orange-600"
+          style={{ color: "#172B4D" }}
+        >
+          The Complete 2022 Flutter & Dart Development Course [Arabic]
+        </Link>
+      </AboutMeCard>
       <Footer />
     </div>
   );
