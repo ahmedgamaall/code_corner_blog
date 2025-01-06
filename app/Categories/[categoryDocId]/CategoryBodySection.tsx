ebbae9b191fe0b56d775d1b6e4b4010c000a3667
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2025-01-06 14:59:56 +0300

    fix category

--- app/Categories/[categoryDocId]/CategoryBodySection.tsx
+++ app/Categories/[categoryDocId]/CategoryBodySection.tsx
@@ -1,5 +1,5 @@
 import ArticlesGrid from "@/app/components/Home/ArticlesGrid";
-import { getArticles } from "@/services/db";
+import { getArticles, getArticlesByCategory } from "@/services/db";
 
 type CategoryBodySectionProps = {
   title: string;
@@ -8,11 +8,17 @@ type CategoryBodySectionProps = {
 export default async function CategoryBodySection({
   title,
 }: CategoryBodySectionProps) {
-  const articles: any = await getArticles();
+  const articles: any = await getArticlesByCategory(title);
 
   return (
     <div className="mt-6 mb-10">
-      <ArticlesGrid articles={articles} />
+      {articles ? (
+        <ArticlesGrid articles={articles} />
+      ) : (
+        <div className="mb-8 font-bold text-3xl">
+          There is no articles in this category
+        </div>
+      )}
     </div>
   );
 }
--- app/Profile/components/ProfileBodySection.tsx
+++
@@ -1,5 +0,0 @@
-export default function ProfileBodySection() {
-  return (
-    <div className="w-full mb-2 pt-16 flex flex-col space-y-4 px-72 justify-evenly items-center py-8"></div>
-  );
-}
